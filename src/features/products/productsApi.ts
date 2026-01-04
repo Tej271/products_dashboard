8739a5c9e64e8c9037dfe27ac6ab47e83b3b01ed
Tej271 <utu8811@gmail.com>
2026-01-04 21:03:19 +0530

    BASE URL for API

--- src/features/products/productsApi.ts
+++ src/features/products/productsApi.ts
@@ -3,7 +3,9 @@ import { type Product } from '../../types';
 
 export const productsApi = createApi({
   reducerPath: 'productsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com' }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: import.meta.env.VITE_API_BASE_URL 
+  }),
   endpoints: (builder) => ({
     getProducts: builder.query<Product[], void>({
       query: () => '/products',
